b4ba67dfa033c5478ff032830dbee9923a975ebb
Mattxm <mattxm@live.com>
2020-10-12 12:39:12 -0700

    Reworked app foundation. New main page and cleaned up files.

--- App.js
+++ App.js
@@ -8,34 +8,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Load from "./pages/Load";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-// import Main from "./pages/Main";
+import Home from "./pages/Home";
 
 // Firebase Code
 import * as firebase from 'firebase';
 import fireconfig from "./firebase";
 // Initialize Firebase
-firebase.initializeApp(fireconfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(fireconfig);
+}
 // firebase.analytics();
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -44,11 +26,10 @@ function App() {
     
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Load">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Load" component={Load} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} options={{ title: "Home", headerShown: false, }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/library.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+const numColumns = 3;
+const data = [
+    { key: 'A' }, { key: 'B' }, { key: 'C' }, { key: 'D' }, { key: 'E' }, { key: 'F' }, { key: 'G' }, { key: 'H' }, { key: 'I' }, { key: 'J' },
+    { key: 'K' }, { key: 'L' }, { key: 'M' }, { key: 'N' }, { key: 'O' }, { key: 'P' }, { key: 'Q' }, { key: 'R' }, { key: 'S' }, { key: 'T' },
+    { key: 'U' }, { key: 'V' }, { key: 'W' }, { key: 'X' }, { key: 'Y' }, { key: 'Z' },
+
+];
+
+const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+  
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+      data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+      numberOfElementsLastRow++;
+    }
+  
+    return data;
+  };
+
+export default class App extends Component {
+  render() {
+    return (
+        <FlatList
+            data={formatData(data, numColumns)}
+            style={styles.container}
+            numColumns={numColumns}
+            renderItem = {({ item, index }) => {
+                if (item.empty === true) {
+                return <View style={[styles.item, styles.itemInvisible]} />;
+                }
+                return (
+                <View style={styles.item}>
+                    <Text style={styles.itemText}>Book {item.key}</Text>
+                </View>
+                )}}
+        />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginVertical: 0,
+    },
+    item: {
+        backgroundColor: '#333',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        margin: 1.5,
+        height: Dimensions.get('window').width / 3 * 1.5,
+    },
+    itemInvisible: {
+        backgroundColor: 'transparent',
+    },
+    itemText: {
+        color: '#fff',
+    },
+  });
