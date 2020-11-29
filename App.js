1fa64672e86b63f48582cbce3c15b77336fe757b
Mattxm <mattxm@live.com>
2020-11-29 00:19:27 -0800

    Scanner finds book title

--- App.js
+++ App.js
@@ -22,53 +22,13 @@ firebase.auth().onAuthStateChanged(user => {
   // Do other things
 });
 
-const AuthContext = React.createContext();
-
-function HomeScreen() {
-  const { signOut } = React.useContext(AuthContext);
-
-  return (
-    <View>
-      <Text>Signed in!</Text>
-      <Button title="Sign out" onPress={signOut} />
-    </View>
-  );
-}
-
-function SignInScreen() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const { signIn } = React.useContext(AuthContext);
-
-  return (
-    <View>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => signIn({ username, password })} />
-    </View>
-  );
-}
-
 const MainNavigator = createMaterialTopTabNavigator();
 const AuthNavigator = createStackNavigator();
 
 
 const AuthContainer = () => (
   <AuthNavigator.Navigator>
-    <AuthNavigator.Screen name="SignIn" component={SignInScreen} options={{ title: 'Sign in',
-      animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-      }}
-    />
+    <AuthNavigator.Screen mame = 'LoadScreen' component = {Load}/>
   </AuthNavigator.Navigator>
   );
 
@@ -90,84 +50,11 @@ const MainContainer = () => (
   </MainNavigator.Navigator>
   );
 
-export default function App({ navigation }) {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'RESTORE_TOKEN':
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case 'SIGN_IN':
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case 'SIGN_OUT':
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
 
-  React.useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-      let userToken;
-
-      try {
-        userToken = await AsyncStorage.getItem('userToken');
-      } catch (e) {
-        // Restoring token failed
-      }
-
-      // After restoring token, we may need to validate it in production apps
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async data => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-      },
-      signOut: () => dispatch({ type: 'SIGN_OUT' }),
-      signUp: async data => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-      },
-    }),
-    []
-  );
-
+export default function App()  {
   return (
     <NavigationContainer>
-      {state.userToken ? <AuthContainer/> : <MainContainer/>}
+      <MainContainer/>
     </NavigationContainer>
   );
 }
--- components/scanner.js
+++ components/scanner.js
@@ -1,4 +1,129 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
+import { Button, View, Text, StyleSheet, Modal, TextInput, useEffect, useContext, ActivityIndicator, TouchableHighlight } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Google from 'expo-google-app-auth';
+import { fetchUpdateAsync } from 'expo-updates';
+
+var modaldata = "0";
+var bookdata = new XMLHttpRequest();
+var apiKey = "AIzaSyCZVzIOlPJHgjhe7KNsFLmUA2uaZHqlXH4";
+
+export default class ScannerScreen extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.getBook = this.getBook.bind(this);
+  }
+  
+  // Component State
+  state = {
+    hasCameraPermission: null, // if app has permissions to acess camera
+    isScanned: false, // scanned
+    show: false,
+    bookInfo: [],
+  }
+  async componentDidMount() {
+    // ask for camera permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    console.log(status);
+    this.setState({ hasCameraPermission: status === "granted" ? true : false });
+  }
+
+  handleBarCodeScanned = ({ data }) => {
+    var isbn = data;
+    this.setState({ scanned: true });
+    this.getBook(isbn, this.alertInfo);
+  };
+
+  getBook(isbn, callback) {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${encodeURIComponent(isbn)}`)
+        .then(response => response.json())
+        .then(responseJson => {
+            this.setState({ bookInfo: responseJson.items[0].volumeInfo.title});
+            callback();
+        })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  alertInfo = () => { //Event happens after book information is returned by getBook
+    this.showModal();
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+    this.setState({ scanned: false });
+  };
+
+  render(){
+    const { hasCameraPermission, isScanned } = this.state;
+    if(hasCameraPermission === null){
+      console.log("Requesting permission");
+      return (
+        <ActivityIndicator />
+      );
+    }
+
+    if(hasCameraPermission === false){
+      return ( 
+        <View style = {styles.container}>
+         <Text>Camera Permission Required</Text>
+        </View> 
+      );
+    }
+    
+    return (  
+    <View style = {styles.container}>
+      <Modal visible={this.state.show} handleClose={this.hideModal}>
+        <View style = {styles.container}>
+          <Text>Scanned: {this.state.bookInfo}</Text>
+          <Button title="Close" onPress={() => {this.hideModal();}}/>
+        </View>
+      </Modal>
+      <BarCodeScanner
+        onBarCodeScanned = { isScanned ? undefined : this.handleBarCodeScanned }
+        style = {StyleSheet.absoluteFill}
+        />
+    </View>
+    );
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginVertical: 0,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+
+
+/* import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { app } from 'firebase';
@@ -45,4 +170,4 @@ const styles = StyleSheet.create({
     }
   });
 
-export default App;
+export default App; */
