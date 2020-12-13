ed284bd6889cc586f5ad8f9f62751e8e2cca3370
BriSWells <briswells@gmail.com>
2020-12-13 10:02:31 -0800

    updated eslint rules to include complexity

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: "babel-eslint",
+  'parser': 'babel-eslint',
   'env': {
     'browser': true,
     'es2021': true,
@@ -21,12 +21,29 @@ module.exports = {
     'eslint-plugin-jsdoc',
   ],
   'rules': {
-    "max-len": ["error", 80, 2, {
-    ignoreUrls: true,
-    ignoreComments: false,
-    ignoreRegExpLiterals: true,
-    ignoreStrings: false,
-    ignoreTemplateLiterals: false,
+    'max-len': ['error', 80, 2, {
+      'ignoreUrls': true,
+      'ignoreComments': false,
+      'ignoreRegExpLiterals': true,
+      'ignoreStrings': false,
+      'ignoreTemplateLiterals': false,
     }],
+    'array-callback-return': 2,
+    'complexity': ['error', 5],
+    'eqeqeq': 2,
+    'max-statements': [2, 10],
+    'max-statements-per-line': [2, {
+      'max': 1,
+    }],
+    'max-nested-callbacks': [2, 2],
+    'max-depth': [2, {
+      'max': 2,
+    }],
+    'max-lines': [2, 300],
+    'no-eval': 2,
+    'no-return-assign': 2,
+    'no-param-reassign': 2,
+    'no-var': 2,
+    'prefer-const': 2,
   },
 };
