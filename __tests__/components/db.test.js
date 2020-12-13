b2ca1eb097ead1db20daf8bd07d5a63c35cc2caa
BriSWells <briswells@gmail.com>
2020-12-12 19:39:54 -0800

    Added test for library sample function and search function

--- __tests__/components/db.test.js
+++ __tests__/components/db.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Library from '../../components/db.js';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
 
-import Lib from '../../components/db.js';
-
-it('render component scanner', () => {
-    const TextInputComponent = renderer.create(<Lib />).toJSON();
+it('render component Library', () => {
+    const TextInputComponent = renderer.create(<Library />).toJSON();
     expect(TextInputComponent).toMatchSnapshot();
 });
+
+test('Tests Sample Function',  async () => {
+  const instanceOf = renderer.create(<Library />).getInstance();
+  await instanceOf.sample();
+  await instanceOf.componentDidMount();
+  expect(instanceOf.state.library.length).toBe(3)
+})
+
+test('Test title Search',  async () => {
+  const instanceOf = renderer.create(<Library />).getInstance();
+  await instanceOf.sample();
+  await instanceOf.componentDidMount();
+  expect(instanceOf.state.search.length).toBe(0);
+  await instanceOf.searchLibrary("Hunger");
+  expect(instanceOf.state.search.length).toBe(1);
+})
--- __tests__/components/scanner.test.js
+++ __tests__/components/scanner.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import Scanner from '../../components/scanner.js';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
 
 it('render component scanner', () => {
     const TextInputComponent = renderer.create(<Scanner />).toJSON();
     expect(TextInputComponent).toMatchSnapshot();
 });
+
+// it('Testing scanner', async () => {
+//   const instanceOf = renderer.create(<Scanner />).getInstance();
+//   instanceOf.getBook('0439023521', instanceOf.alertInfo);
+//   await expect(instanceOf.state.author).toEqual('Suzanne Collins')
+// });
