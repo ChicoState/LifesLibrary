c509c744d7caea5b8d9a3e3824f63bf8b0f15ed3
BriSWells <briswells@gmail.com>
2020-12-13 10:16:29 -0800

    Added eslintignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     'browser': true,
     'es2021': true,
   },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
   'extends': [
     'plugin:react/recommended',
     'google',
