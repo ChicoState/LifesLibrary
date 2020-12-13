aa2de609bd3f47ee73d0cefdadd8d15cadbcd8a6
BriSWells <briswells@gmail.com>
2020-12-12 22:37:32 -0800

    removed test to fix git merge issues

--- __tests__/components/scanner.test.js
+++ __tests__/components/scanner.test.js
@@ -4,16 +4,27 @@ import Scanner from '../../components/scanner.js';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
+<<<<<<< HEAD
 
 Enzyme.configure({ adapter: new Adapter() })
+=======
+>>>>>>> c04c2eb298305a40dff152e7f5f3688479243716
 
 it('render component scanner', () => {
     const TextInputComponent = renderer.create(<Scanner />).toJSON();
     expect(TextInputComponent).toMatchSnapshot();
 });
 
+<<<<<<< HEAD
 // it('Testing scanner', async () => {
 //   const instanceOf = renderer.create(<Scanner />).getInstance();
 //   instanceOf.getBook('0439023521', instanceOf.alertInfo);
 //   await expect(instanceOf.state.author).toEqual('Suzanne Collins')
 // });
+=======
+test('Tests Sample Function',  async () => {
+  const instanceOf = renderer.create(<Scanner />).getInstance();
+  await instanceOf.getBook('0439023521', instanceOf.alertInfo);
+  expect(instanceOf.state.library.author).toBe('Suzanne Collins')
+})
+>>>>>>> c04c2eb298305a40dff152e7f5f3688479243716
